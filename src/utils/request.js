ee16976a6e803d7b0df8bb840d1f7f6cd8dd79ac
Jeric Frank Sasil <jericfrank@gmail.com>
2017-12-14 01:43:24 +0800

    refactor(request): server uri

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,2 +1,4 @@
-export const PAGES_URI = `http://ec2-13-59-87-238.us-east-2.compute.amazonaws.com`;
-export const HOST_URI = `http://ec2-13-59-87-238.us-east-2.compute.amazonaws.com:8080`;
+const HOST = 'ec2-13-59-87-238.us-east-2.compute.amazonaws.com';
+
+export const PAGES_URI = `http://${HOST}`;
+export const HOST_URI  = `http://${HOST}:8080`;
